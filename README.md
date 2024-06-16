2f4185b6cec2991983f356cf015ae3e2f8c6b807
Ap0ll0 <Ap0ll0Media@protonmail.com>
2024-06-15 20:39:38 -0700

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Beauty of Annihilation Call of Cthulhu Campaign Organizer
+I wanted to organize my Call of Cthulhu game a bit better, and most of the notes apps didn't give me the organization I wanted. I am hoping to make this website organize my game better. Should it prove quite useful and robust, I will consider launching a version that helps you build a campaign/game on your own, with no code to be friendly to my fellow RPG tabletop fans
+# Tools
+- I am trying **TypeScipt** instead of JavaScript for the first time on this site
+- I am using React, and trying the react router dom to intercept web requests and go to different web pages
+- I am using Affinity Photo 2 to help with images and boost visual appeal
+
+# What is Call of Cthulhu
+Call of Cthulhu is a role playing game, similar to Dungeons and Dragons for the uninitiated. While I am considerably new to this game, it is about cosmic and eldritch horrors, with an emphasis on dread and terror, contradicting the often uplifting or heroic themes of D&D. In designing a game or campaign, there are many aspects to consider, including the world, (from my naive perspective, it appears most games take place on earth, rather than another world, as can be more common in D&D). 
+
+History and lore of the world can be quite different if needed, but for my game, I am keeping the general history of Earth, and most games take place a bit more in the future compared to D&D, in the 19th and 20th centuries, generally the 1920s are used in the games I have played. 
+
+# Wrapping Up
+Thanks for viewing this Repo, I hope that this website, or some variant more suitable for reading, rather than organizing my personal game, may be published and viewable in the future.
+*This repo has omitted the Assets folder, as it contains copyrighted imagery that may be allowed for personal, but not public use.*
--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,14 +1,16 @@
 function Header() {
     return (
         <div className="homePage">
-            <h1 id="title">Beauty of Annihilation</h1>
+            <h1 id="title" >Beauty of Annihilation</h1>
+            <hr id="titleBar"></hr>
+            <img src="../Assets/HistoryCards.png" className="Cards" id="historyCard"/>
             <h2 className="homeText">The Auction House</h2>
             <p className="homeText">You have all been invited to the auction house, the investigators must arrive to Paris and to the auction by 1800 hours, where everyone will meet for the first time. Perhaps by unfortuante circumstances, or by enjoyable banter prior
-                to the auction. The auction is eventually called into session after a bit, and has some items for auction, the night starts out with the following.</p>
+                to the auction. The auction is eventually called into session after a bit, and has some items for auction, the night starts out with the following: </p>
             <ul className="homeText">
-                <li className="homeText">The Skull of Saint Peter</li>
-                <li className="homeText">The Last Will and Testament of Merlin</li>
-                <li className="homeText">Orders of Nightmares</li>
+                <li className="homeText"><span>The Skull of Saint Peter</span></li>
+                <li className="homeText"><span>The Last Will and Testament of Merlin</span></li>
+                <li className="homeText"><span>Orders of Nightmares</span></li>
             </ul>
             <p className="homeText">Many interested bidders are happy to bring the bidding up to hundreds of dollars, most likely leaving all of our players out of the bidding. An intermission is granted and guests are encouraged to eat and drink, finally
                 there are more hauntings to start. If some players have the perception or notice, there are some groans they might here. Or they might spot a small body part or bit of flesh on the ground or hidden inside a plant.
