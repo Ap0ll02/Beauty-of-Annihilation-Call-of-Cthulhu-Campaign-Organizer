40b94d795041ed8213747611003e44e5cbd12888
Ap0ll0 <Ap0ll0Media@protonmail.com>
2024-06-15 17:40:46 -0700

    Initial Commit

---
+++ src/Components/header.tsx
@@ -0,0 +1,26 @@
+function Header() {
+    return (
+        <div className="homePage">
+            <h1 id="title">Beauty of Annihilation</h1>
+            <h2 className="homeText">The Auction House</h2>
+            <p className="homeText">You have all been invited to the auction house, the investigators must arrive to Paris and to the auction by 1800 hours, where everyone will meet for the first time. Perhaps by unfortuante circumstances, or by enjoyable banter prior
+                to the auction. The auction is eventually called into session after a bit, and has some items for auction, the night starts out with the following.</p>
+            <ul className="homeText">
+                <li className="homeText">The Skull of Saint Peter</li>
+                <li className="homeText">The Last Will and Testament of Merlin</li>
+                <li className="homeText">Orders of Nightmares</li>
+            </ul>
+            <p className="homeText">Many interested bidders are happy to bring the bidding up to hundreds of dollars, most likely leaving all of our players out of the bidding. An intermission is granted and guests are encouraged to eat and drink, finally
+                there are more hauntings to start. If some players have the perception or notice, there are some groans they might here. Or they might spot a small body part or bit of flesh on the ground or hidden inside a plant.
+                Even a bit of blood trails might be seen by the investigators. They can attempt to sneak around the house the auction is held at and explore for more clues. 
+            </p>
+            <br></br>
+            <h3 className="homeText">Choosing to explore</h3>
+            <p className="homeText">Should our adventurers decide to explore, the house will have an upstairs where the auction is not being held, but where the blood trail would lead to. If successfully followed, not without darkening skies, and perhaps lightning. Other frightening events will occur. 
+                Upon entering the room the blood trail leads to, there will be a dead body in a room. Not a zombie... yet, as this man is lying on the bed, bled out and cold to the touch.
+            </p>
+            <h2 className="homeText">Auctioning off the forbidden item</h2>
+        </div>
+    )
+}
+export default Header
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./Components/header.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
